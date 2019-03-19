ed5695cb8d3d1e7cf8db0872bdc5f0446e5ca402
iankabugi <iank299@gmail.com>
2019-03-19 09:12:29 +0300

    add necessary forms

---
+++ awards/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Project, Profile, DesignRating, UsabilityRating, ContentRating
+
+
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        exclude = ['user','profile','posted_on']
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
+class DesignForm(forms.ModelForm):
+   class Meta:
+       model = DesignRating
+       fields = ['rating']
+
+class UsabilityForm(forms.ModelForm):
+   class Meta:
+       model = UsabilityRating
+       fields = ['rating']
+
+class ContentForm(forms.ModelForm):
+   class Meta:
+       model = ContentRating
+       fields = ['rating']
