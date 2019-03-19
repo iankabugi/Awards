97b23b9a4e996ac2a88a461a4d61d2635eb1f258
iankabugi <iank299@gmail.com>
2019-03-19 09:11:37 +0300

    add your models to admin to achieve admin functionality

--- awards/admin.py
+++ awards/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile, Project, DesignRating, ContentRating, UsabilityRating
 
-# Register your models here.
+admin.site.register(Profile)
+admin.site.register(Project)
+admin.site.register(DesignRating)
+admin.site.register(ContentRating)
+admin.site.register(UsabilityRating)
